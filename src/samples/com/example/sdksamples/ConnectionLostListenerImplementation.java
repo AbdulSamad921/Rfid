4eefef8c39a5cadf2b3d71b91c135fc1c340b9df
AbdulSamad921 <ask428@live.com>
2020-04-10 16:22:36 -0700

    Initial commit

---
+++ src/samples/com/example/sdksamples/ConnectionLostListenerImplementation.java
@@ -0,0 +1,15 @@
+package samples.com.example.sdksamples;
+
+import com.impinj.octane.ConnectionLostListener;
+import com.impinj.octane.ImpinjReader;
+
+public class ConnectionLostListenerImplementation implements
+        ConnectionLostListener {
+
+    @Override
+    public void onConnectionLost(ImpinjReader reader) {
+        System.out.println("Connection Lost: Disconnecting");
+        reader.disconnect();
+        System.exit(0);
+    }
+}
